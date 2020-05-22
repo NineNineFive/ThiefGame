e671b7d56ff400cd065c922be567e332ea6a191f
NineNineFive <38846235+NineNineFive@users.noreply.github.com>
2020-05-22 11:24:48 +0200

    Final Release (before bug testing)

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public static UserInterface instance;
+    public bool empty = false;
+    public float cash;
+    public string name;
+    
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (empty) {
+            UserInterface.instance.toolText.text = "Empty Inventory Slot";
+        } else {
+            UserInterface.instance.toolText.text = name + "\nWorth: " + cash + "$";
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        UserInterface.instance.toolText.text = "";
+    }
+}
