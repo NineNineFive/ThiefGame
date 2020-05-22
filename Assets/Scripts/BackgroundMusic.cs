eda1fee281300a56573dd4e747de488e2b510ef9
NineNineFive <38846235+NineNineFive@users.noreply.github.com>
2020-05-22 11:26:15 +0200

    Final Release (before bug testing)

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class BackgroundMusic : MonoBehaviour {
+    private static BackgroundMusic instance;
+    void Awake() {
+        if(instance==null) {
+	        instance = this;
+            instance.GetComponent<AudioSource>().Play();
+        }
+        DontDestroyOnLoad(instance.gameObject);
+    }
+}
